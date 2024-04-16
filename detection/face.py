4cb406b55fde9377c9d1b244e8c49924ea665ca8
nate-q-136 <lqnhat136@gmail.com>
2024-04-16 14:07:24 +0700

    Update Feat: Use Yunet for Face detection Add: Register Form

--- detection/face.py
+++ detection/face.py
@@ -1,12 +1,25 @@
-from deepface import DeepFace
-import cv2
+import cv2     
 from skimage import data
 from skimage.feature import Cascade
+import numpy as np
 
+# cascade face detection
 trained_file = data.lbp_frontal_face_cascade_filename()
-haar_cascade = cv2.CascadeClassifier("/Volumes/Untitled 2/2-PPBL-Cham-Cong/timekeeping_realtime/detection/haarcascade_frontalface_default.xml") 
+haar_cascade = cv2.CascadeClassifier("/Volumes/Untitled 2 1/2-PPBL-Cham-Cong/timekeeping_realtime/detection/haarcascade_frontalface_default.xml") 
 
 face_detector = Cascade(trained_file)
+
+# dnn face detection
+yunet = cv2.FaceDetectorYN.create(
+    model="/Volumes/Untitled 2 1/2-PPBL-Cham-Cong/timekeeping_realtime/detection/face_detection_yunet_2023mar.onnx",
+    config='',
+    input_size=(320, 320),
+    score_threshold=0.6,
+    nms_threshold=0.3,
+    top_k=5000,
+    backend_id=cv2.dnn.DNN_BACKEND_DEFAULT,
+    target_id=cv2.dnn.DNN_TARGET_CPU
+)
 
 class FaceDetector:
     def __init__(self, image_path, store):
@@ -20,52 +33,15 @@ class FaceDetector:
         # faces = face_detector.detect_multi_scale(image=image, scale_factor=1.2, step_ratio=1, min_size=(50, 50), max_size=(200, 200))
         return faces
     
-    def detect(self):
-        metrics = ["cosine", "euclidean", "euclidean_l2"]
-        backends = [
-        'opencv', 
-        'ssd', 
-        'dlib', 
-        'mtcnn', 
-        'fastmtcnn',
-        'retinaface', 
-        'mediapipe',
-        'yolov8',
-        'yunet',
-        'centerface',
-        ]
-        dfs = DeepFace.find(img_path=self.image_path, db_path=self.store, distance_metric=metrics[0])
-        
-        data_faces = []
-        for df in dfs:
-            target_x = df["target_x"].values[0]
-            target_y = df["target_y"].values[0]
-            target_w = df["target_w"].values[0]
-            target_h = df["target_h"].values[0]
-
-            target_x2 = target_x + target_w
-            target_y2 = target_y + target_h
-            # source_x = df["source_x"].values[0]
-            # source_y = df["source_y"].values[0]
-            # source_w = df["source_w"].values[0]
-            # source_h = df["source_h"].values[0]
-            # threshold = df["threshold"].values[0]
-            # distance = df["distance"].values[0]
-
-            data_info = {
-                "target_x": target_x,
-                "target_y": target_y,
-                "target_x2": target_x2,
-                "target_y2": target_y2,
-                "target_w": target_w,
-                "target_h": target_h,
-                # "source_x": source_x,
-                # "source_y": source_y,
-                # "source_w": source_w,
-                # "source_h": source_h,
-                # "threshold": threshold,
-                # "distance": distance,
-            }
-            data_faces.append(data_info)
-        return data_faces
-        pass
+    @staticmethod
+    def detect_v3(image):
+        yunet.setInputSize((image.shape[1], image.shape[0]))
+        _, faces = yunet.detect(image)
+        coors_faces = []
+        for face in faces:
+            bbox = face[0:4].astype(np.int32)
+            coors_faces.append(bbox)
+        return coors_faces
+    
+    
+  
--- main_timekeeping.py
+++ main_timekeeping.py
@@ -1,4 +1,4 @@
-from tkinter import Button, Label, E
+from tkinter import Button, Label, E,  Entry, messagebox
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
@@ -6,7 +6,9 @@ import uuid
 from detection.face import FaceDetector
 from utils.drawing import ImageDrawing
 import os
+import numpy as np
 
+os.makedirs("captured_images", exist_ok=True)
 
 class TimeKeepingApp:
     def __init__(self, window:tk.Tk, window_title, window_size="800x800"):
@@ -20,6 +22,9 @@ class TimeKeepingApp:
         # Thông báo
         self.label = tk.Label(window, text="Chấm công", font=("Arial", 16))
         self.label.pack()
+
+        self.label_info = tk.Label(window, text="", font=("Arial", 12))
+        self.label_info.place(x=400, y=700)
 
         # Hiển thị camera
         self.canvas_width = self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -39,14 +44,27 @@ class TimeKeepingApp:
         self.canvas.pack()
 
         # Nút bấm
-        self.btn_1 = Button(window, text="Start Camera", width=20, command=self.start_camera)
-        self.btn_1.pack(side=tk.LEFT, padx=20, pady=20)
+        self.btn_start_camera = Button(window, text="Start Camera", width=20, command=self.start_camera)
+        self.btn_start_camera.place(x=20, y=20)  # Đặt ở tọa độ (20, 20)
 
-        self.btn_2 = Button(window, text="Stop Camera", width=20, command=self.stop_camera)
-        self.btn_2.pack(side=tk.RIGHT, padx=20, pady=20)
+        self.btn_stop_camera = Button(window, text="Stop Camera", width=20, command=self.stop_camera)
+        self.btn_stop_camera.place(x=760, y=20)  # Đặt ở tọa độ (760, 20)
 
+        self.btn_capture = Button(window, text='Capture Image', width=20, command=self.capture_image)
+        self.btn_capture.place(x=20, y=50)  # Đặt ở tọa độ (20, 50)
+
+        self.btn_checkin = Button(window, text="Checkin", width=20, command=self.checkin)   
+        self.btn_checkin.place(x=20, y=760)  # Đặt ở tọa độ (760, 50)
+
+        self.btn_checkout = Button(window, text="Checkout", width=20, command=self.checkout)
+        self.btn_checkout.place(x=760, y=760)  # Đặt ở tọa độ (760, 50)
+
+        self.btn_register = Button(window, text="Register", width=20, command=self.show_form_register)
+        self.btn_register.place(x=400, y=760)  # Đặt ở tọa độ (760, 50)
         # Biến kiểm soát camera
         self.is_camera_running = False
+
+        global captured_image
 
         # Khởi tạo vòng lặp chạy
         self.update()
@@ -60,29 +78,61 @@ class TimeKeepingApp:
     def stop_camera(self):
         self.is_camera_running = False
 
+    def capture_image(self):
+        global captured_image
+        if captured_image.any():
+            # Lưu ảnh
+            image_path = f"captured_images/{uuid.uuid4()}.jpg"
+            cv2.imwrite(image_path, captured_image)
+            print(f"Saved image to {image_path}")
+    
+    def show_form_register(self):
+        form = tk.Toplevel(self.window)
+        form.title("Information Form")
+        form.geometry("300x150")
+        
+        # Labels and Entries
+        label_name = Label(form, text="Name:")
+        label_name.grid(row=0, column=0, padx=5, pady=5)
+        entry_name = Entry(form)
+        entry_name.grid(row=0, column=1, padx=5, pady=5)
+        
+        label_id = Label(form, text="ID:")
+        label_id.grid(row=1, column=0, padx=5, pady=5)
+        entry_id = Entry(form)
+        entry_id.grid(row=1, column=1, padx=5, pady=5)
+        
+        # Buttons
+        btn_ok = Button(form, text="OK", command=form.destroy)
+        btn_ok.grid(row=2, column=0, padx=5, pady=5)
+        
+        btn_cancel = Button(form, text="Cancel", command=form.destroy)
+        btn_cancel.grid(row=2, column=1, padx=5, pady=5)
+    
+    def checkin(self):
+        print("Checkin")
+
+    def checkout(self):
+        print("Checkout")
+
+    
+
     def update(self):
+        global captured_image
         if self.is_camera_running:
             ret, frame = self.camera.read()
             if ret:
                 # Resize frame từ camera
-                frame = cv2.resize(frame, (self.canvas_width, self.canvas_height))
-                name_temp_image = f"image_{uuid.uuid4()}.jpg"
-                store = "temp_images"
-                # get full path store
-                full_path_store = os.path.join(os.path.dirname(__file__), store)
-                print("full_path_store", full_path_store)
-                cv2.imwrite(f"{store}/{name_temp_image}", frame)
+                frame:np.ndarray = cv2.resize(frame, (self.canvas_width, self.canvas_height))
                 try:
                     
-                    data_faces = FaceDetector.detect_v2(frame)
-                    print("data_faces", data_faces)
-                    # face_detector = FaceDetector(image_path=f"{store}/{name_temp_image}", store=full_path_store)
-                    # data_faces = face_detector.detect()
+                    data_faces = FaceDetector.detect_v3(frame)
+                    num_faces = len(data_faces)
+                    # set label number of faces
+                    self.label_info.config(text=f"Chấm công - Số khuôn mặt: {num_faces}")
                     for (x, y, w, h) in data_faces:
-                        # if data_face:
-                            # x1, y1, x2, y2 = data_face["target_x"], data_face["target_y"], data_face["target_x2"], data_face["target_y2"]
                         x1, y1, x2, y2 = x, y, x+w, y+h
-                        print("x1, y1, x2, y2", x1, y1, x2, y2)
+                        captured_image = frame[y1:y2, x1:x2].copy()
                         frame = ImageDrawing.draw_rectangle(frame, x1=x1, y1=y1, x2=x2, y2=y2)
                     
                     # Chuyển đổi frame cv2 sang PIL
@@ -90,7 +140,6 @@ class TimeKeepingApp:
                     image = Image.fromarray(image)
                     image = ImageTk.PhotoImage(image)
 
-                    os.remove(f"{store}/{name_temp_image}")
                     # Hiển thị hình ảnh trên canvas
                     if hasattr(self, 'canvas'):
                         self.canvas.create_image(0, 0, image=image, anchor=tk.NW)
@@ -105,13 +154,13 @@ class TimeKeepingApp:
                     if hasattr(self, 'canvas'):
                         self.canvas.create_image(0, 0, image=image, anchor=tk.NW)
                         self.canvas.image = image
-                    os.remove(f"{store}/{name_temp_image}")
                     pass
             
         self.window.after(5, self.update)
     
     def on_closing(self):
         self.is_camera_running = False
+        self.window.destroy()
 
 root = tk.Tk()
 app = TimeKeepingApp(root, "Timekeeping App")
